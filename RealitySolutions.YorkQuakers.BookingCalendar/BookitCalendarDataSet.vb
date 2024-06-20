ab6d0d740a7e3a37393494dfbae8ff4c869ed4ee
Mark Wilson <158046904+KanjiGuru@users.noreply.github.com>
2024-06-20 14:46:50 +0100

    Initial Commit
    Initial Commit

--- RealitySolutions.YorkQuakers.BookingCalendar/BookitCalendarDataSet.vb
+++
@@ -1,2 +0,0 @@
-﻿Partial Class BookitCalendarDataSet
-End Class
--- RealitySolutions.YorkQuakers.BookingCalendar/Modules/modGlobal.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports SageDataObject240
-
-Module modGlobal
-    Public Property Sage50Accounts As List(Of String)
-
-
-End Module
